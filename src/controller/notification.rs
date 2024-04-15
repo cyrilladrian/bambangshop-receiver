f17017d1ac1c02fa43aae7da9ebef112f1799149
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-04-15 20:35:14 +0700

    Implement list function in Notification controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -18,12 +18,20 @@ pub fn unsubscribe(product_type: &str) -> Result<Json<SubscriberRequest>>{
     return match NotificationService::unsubscribe(product_type){
         Ok(f) => Ok(Json::from(f))
         Err(e) => Err(e)
-    }
+    };
 }
 
 #[post("/receive", data="<notification>")]
 pub fn receive(notification: Json<Notification>) -> Result<Json<Notification>> {
     return match NotificationService::receive_notification(notification.into_inner()){
+        Ok(f) => Ok(Json::from(f)), 
+        Err(e) => Err(e)
+    };
+}
+
+#[get("/")]
+pub fn list() -> Result<Json<Vec<String>>>{
+    return match NotificationService::list_messages(){
         Ok(f) => Ok(Json::from(f)), 
         Err(e) => Err(e)
     };
